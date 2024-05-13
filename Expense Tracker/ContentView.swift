180dbaa5a7656646e036cd5708dbb20010797181
Guilherme Paiva <guilhermepaiva9@gmail.com>
2024-05-13 09:11:29 -0300

    Adds tab view

--- Expense Tracker/ContentView.swift
+++ Expense Tracker/ContentView.swift
@@ -9,53 +9,41 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
+    /// Intro Visibility Status
+    @AppStorage("isFirstTime") private var isFirstTime: Bool = true
+    
+    /// Active Tab
+    @State private var activeTab: Tab = .recents
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        TabView(selection: $activeTab) {
+            Text("Recents")
+                .tag(Tab.recents)
+                .tabItem { Tab.recents.tabContent }
+            
+            Text("Search")
+                .tag(Tab.search)
+                .tabItem { Tab.search.tabContent }
+            
+            Text("Chart")
+                .tag(Tab.charts)
+                .tabItem { Tab.charts.tabContent }
+            
+            Text("Settings")
+                .tag(Tab.settings)
+                .tabItem { Tab.settings.tabContent }
         }
+        .sheet(isPresented: $isFirstTime, content: {
+            IntroScreen()
+                .interactiveDismissDisabled()
+        })
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        
 }
